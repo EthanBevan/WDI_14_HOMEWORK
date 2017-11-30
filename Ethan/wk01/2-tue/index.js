24b797f32a3ecbec35a1f390710326b65dfc1de4
Ethan <ethanbevan00@yahoo.com>
2017-11-30 11:01:59 +1100

    homework tuesday/wednesday

---
+++ Ethan/wk01/2-tue/index.js
@@ -0,0 +1,59 @@
+/*//One
+
+var name = prompt("what is your name?") 
+	
+console.log('Wassup ' + name)
+
+*/
+
+/*//Two
+
+var menuChoice = prompt('What would you like to order');
+if (menuChoice ==='fruit salad' || menuChoice ==='tofurkey'){
+	console.log('This cuisin is vegan friendly')
+} else {
+ console.log('Vegans beware')
+}
+
+*/
+
+/*//Three
+
+var numberOfChildren = 10
+var partnerName = ' Amanda'
+var geographicalLocation = ' California'
+var jobTitle = ' Entrepreneur'
+
+console.log(numberOfChildren + partnerName  + geographicalLocation + jobTitle)
+
+*///Four
+/*
+
+var currentYear = 2017
+var birthYear = 1997
+var ageone = 20
+var agetwo = 19
+
+console.log('They are either ' + agetwo + ' or ' + ageone)
+
+*///Five 
+
+
+var currentAge = 20
+var maximumAge = 100
+var estimatedPerDay = 50
+var totalPerYear = estimatedPerDay * 365
+
+console.log('You will need ' + (maximumAge - currentAge) * totalPerYear + ' to last you until the ripe old age of ' + maximumAge)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Ethan/wk01/3-wed/Arrays/arrays.js
@@ -0,0 +1,18 @@
+/*
+var days_of_the_week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday','saturday', 'sunday'];
+
+days_of_the_week.pop();
+
+days_of_the_week.unshift('sunday');
+
+*/
+/*
+
+var days_of_the_week = [["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"], ["Saturday", "Sunday"]]
+
+days_of_the_week.pop(['saturday', 'sunday'])
+
+*/
+
+var days_of_the_week = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+days_of_the_week.sort();
